2a91fcc46ac7e7eee5542e18394a8604a36eaa09
tombrowngithub <tombrownanuma@gmail.com>
2024-08-14 04:17:16 +0100

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from "react-native"
+import {SafeAreaView} from "react-native-safe-area-context";
+
+const Index = () => {
+    return (
+        <SafeAreaView className="w-full h-full justify-center items-center">
+            <Text>Index home</Text>
+        </SafeAreaView>
+    )
+}
+export default Index
